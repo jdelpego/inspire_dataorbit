0a829dfc75bbdcad4b815b8f1b985f635f1fb88c
samc005 <113663437+samc005@users.noreply.github.com>
2025-02-22 10:27:13 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.set_page_config(page_title="NFLPredictor", page_icon=":football:", layout="wide")
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
