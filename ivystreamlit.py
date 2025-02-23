5ba2af788ca9b56743ffe3637831f1b655ba1678
Ivy Holiday <iholiday@ucsb.edu>
2025-02-23 10:34:37 -0800

    reversed previous change

--- ivystreamlit.py
+++ ivystreamlit.py
@@ -5,7 +5,7 @@ query_params = st.query_params
 lat = query_params.get("lat", [""])[0]
 lon = query_params.get("lon", [""])[0]
 elevation = query_params.get("elevation", [""])[0]
-flooding_year = query_params.get("flooding_year", [""])[0]
+flood_year = query_params.get("flood_year", [""])[0]
 years_until = query_params.get("years_until", [""])[0] 
 
 st.markdown(
@@ -85,7 +85,7 @@ with col1:
     st.image("city.jpg", width=200)
 with col2:
     st.markdown(f'<div class="details"><span style="color: #00796B; font-weight: bold;">Coordinates: Latitude: {lat}, Longitude: {lon}</span></div>', unsafe_allow_html=True)
-    st.markdown(f'<div class="sea-level">Sea Level: {elevation}</div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="sea-level">Sea Level: {elevation} m</div>', unsafe_allow_html=True)
 
 # Info box with Time to Sink and Time Remaining in columns
 with st.container():
@@ -96,7 +96,7 @@ with st.container():
     
     with info_col1:
         st.markdown('<div class="info-item">Submersion Year</div>', unsafe_allow_html=True)
-        st.markdown(f'<div class="info-value">{flooding_year}</div>', unsafe_allow_html=True)
+        st.markdown(f'<div class="info-value">{flood_year}</div>', unsafe_allow_html=True)
     with info_col2:
         st.markdown('<div class="info-item">Time Remaining</div>', unsafe_allow_html=True)
         st.markdown(f'<div class="info-value">{years_until}</div>', unsafe_allow_html=True)
