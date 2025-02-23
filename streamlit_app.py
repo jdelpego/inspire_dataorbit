b15590b3d5d7eb52369f67e66e379fe6acc0fdab
jdelpego <jdelpego2@gmail.com>
2025-02-23 09:08:28 -0800

    Added sus to error message

--- streamlit_app.py
+++ streamlit_app.py
@@ -205,9 +205,9 @@ def get_elevation(lat, lon):
         if result["status"] == "OK":
             return result["results"][0]["elevation"]
         else:
-            raise Exception("Status not OK getting elevation" + str(result))
+            raise Exception("Status not OK getting elevation" + str(result) + google_maps_api_key)
     else:
-        raise Exception("Error getting elevation 200 response" + str(result))
+        raise Exception("Error getting elevation 200 response" + str(result) + google_maps_api_key)
         return None
 
 @st.cache_data
