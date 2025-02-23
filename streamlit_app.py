7675195dc38860c549d5fa00ab0076de1cf38a62
Luke Herbelin <lukeherbelin@ucsb.edu>
2025-02-23 13:15:39 -0800

    got rid of flooding year

--- streamlit_app.py
+++ streamlit_app.py
@@ -139,10 +139,6 @@ def predict_flooding_year(altitude_mm, model, future_X, base_sea_level, start_ye
     flooding_year = years_needed[flooding_levels][0]
     years_until_flooding = flooding_year - start_year
 
-    # TEMP
-    years_until_flooding *= 2.5
-    flooding_year = years_until_flooding + start_year
-
     return flooding_year, years_until_flooding
 
 
