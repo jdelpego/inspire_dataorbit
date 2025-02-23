2849e24bb92827c75f39a7e75f15b7253971a938
Ivy Holiday <iholiday@ucsb.edu>
2025-02-23 09:29:05 -0800

    updated page to include variables from main page

--- ivystreamlit.py
+++ ivystreamlit.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import urllib.parse
+
+query_params = st.query_params
+lat = query_params.get("lat", [""])[0]
+lon = query_params.get("lon", [""])[0]
+elevation = query_params.get("elevation", [""])[0]
+flood_year = query_params.get("flood_year", [""])[0]
+years_until = query_params.get("years_until", [""])[0] 
 
 st.markdown(
     """
@@ -43,7 +51,6 @@ st.markdown(
         }
         .info-box {
             background-color: #B3E5FC;
-            #padding: 100px;
             border-radius: 5px;
             margin-top: 20px;
         }
@@ -77,8 +84,8 @@ col1, col2 = st.columns([1, 3])
 with col1:
     st.image("city.jpg", width=200)
 with col2:
-    st.markdown('<div class="details"><span style="color: #00796B; font-weight: bold;">Coordinates: Latitude: xx.xx, Longitude: yy.yy</div>', unsafe_allow_html=True)
-    st.markdown('<div class="sea-level">Sea Level: 10m</div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="details"><span style="color: #00796B; font-weight: bold;">Coordinates: Latitude: {lat}, Longitude: {lon}</span></div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="sea-level">Sea Level: {elevation}</div>', unsafe_allow_html=True)
 
 # Info box with Time to Sink and Time Remaining in columns
 with st.container():
@@ -89,11 +96,11 @@ with st.container():
     
     with info_col1:
         st.markdown('<div class="info-item">Submersion Year</div>', unsafe_allow_html=True)
-        st.markdown('<div class="info-value">2030</div>', unsafe_allow_html=True)
+        st.markdown(f'<div class="info-value">{flood_year}</div>', unsafe_allow_html=True)
     
     with info_col2:
         st.markdown('<div class="info-item">Time Remaining</div>', unsafe_allow_html=True)
-        st.markdown('<div class="info-value">10 years</div>', unsafe_allow_html=True)
+        st.markdown(f'<div class="info-value">{years_until}</div>', unsafe_allow_html=True)
 
     st.markdown('</div>', unsafe_allow_html=True)
 
